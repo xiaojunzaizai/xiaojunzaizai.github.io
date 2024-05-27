83501ae08cee50f6e4d598e674b394628a40799a
xiaojunzaizai <xiaojunzaizai@gmail.com>
2024-05-27 14:09:28 -0400

    更新主页面

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var heading = document.querySelector('h1');
     if (heading) {
-        heading.textContent = 'Hello, TypeScript and GitHub Pages!';
+        heading.textContent = '欢迎来到小珺仔的工具屋';
     }
 });
